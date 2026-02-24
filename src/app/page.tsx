d1355ed75922a54669ec5f6b519ebdd77360077b
Anikaz23 <anikaitkadamb@icloud.com>
2026-02-24 16:15:29 -0500

    chore: sync pending workspace changes (batch 7/12)

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import HomeDigestsLoader from "@/components/home-digests-loader";
-
-export default function HomePage() {
-  return <HomeDigestsLoader />;
-}
--- src/app/settings/page.tsx
+++
@@ -1,5 +0,0 @@
-import SettingsLoader from "@/components/settings-loader";
-
-export default function SettingsPage() {
-  return <SettingsLoader />;
-}
